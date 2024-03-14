65ef6c86a7cdebf7681130285388b90730638a2a
FlamesNekkys <yodacj21@gmail.com>
2024-03-15 02:33:01 +0300

    add pipeline

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-const config = require('./utils/config')
-const express = require('express')
-require('express-async-errors')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const config = require('./utils/config');
+const express = require('express');
+require('express-async-errors');
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-logger.info('connecting to', config.MONGO_URL)
+logger.info('connecting to', config.MONGO_URL);
 
 mongoose
     .connect(config.MONGO_URL)
 
     .then((result) => {
-        logger.info('connected to MongoDB')
+        logger.info('connected to MongoDB');
     })
     .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
+        logger.error('error connecting to MongoDB:', error.message);
+    });
 
-app.use(cors())
-app.use(express.json())
-app.use(middleware.reqLogger)
-app.use(middleware.tokenExtractor)
+app.use(express.static('/bloglist-frontend/dist'));
 
-app.use('/api/blogs', middleware.userExtractor, blogsRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use(cors());
+app.use(express.json());
+app.use(middleware.reqLogger);
+app.use(middleware.tokenExtractor);
+
+app.use('/api/blogs', middleware.userExtractor, blogsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 if (process.env.NODE_ENV === 'test') {
-    const testingRouter = require('./controllers/testing')
-    app.use('/api/testing', testingRouter)
+    const testingRouter = require('./controllers/testing');
+    app.use('/api/testing', testingRouter);
 }
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
